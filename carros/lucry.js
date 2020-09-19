a5fdb5cd9cd18913781db33ee76e17c556c241ab
Jefferson Martins de Andrade <jeffersonmandrade@gmail.com>
2020-09-19 04:09:11 -0300

    criação da atividade carros

---
+++ carros/lucry.js
@@ -0,0 +1,31 @@
+export const nome = "lucry"
+
+export function temCarroDaMarca(carros, marca) {
+    // coloque aqui o algoritmo para retornar um boolean (true ou false)
+    // se existir um carro com a marca informada ou não
+    return null
+}
+
+export function carrosNaCor(carros, cor) {
+    // coloque aqui o algoritmo para retornar a quantidade
+    // de carros na cor recebida como parâmetro
+    return null
+}
+
+export function anosDisponiveis(carros) {
+    // coloque aqui o algoritmo para retornar um array de number
+    // com os anos disponíveis nos carros informados
+    return null
+}
+
+export function tiposDisponiveis(carros) {
+    // coloque aqui o algoritmo para retornar um array de strings
+    // com os tipos disponíveis nos carros informados
+    return null
+}
+
+export function selecionarCarrosDaMarca(carros, marca) {
+    // coloque aqui o algoritmo para retornar um array de object
+    // com os carros da marca informado como parâmetro
+    return null
+}
