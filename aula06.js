4c2df1c40d1cd33097bea72d7de116c45a7680b3
Lucryhbell <lucryhbell80gmail.com>
2020-09-19 15:48:42 -0500

    adicionando archivo aula06.js

---
+++ aula06.js
@@ -0,0 +1,146 @@
+const class1 = ['yasmin', 'isadora', 'benedita', 'brenda', 'luiza', 'fatima', 'caio', 
+'teresinha', 'hugo', 'zeca', 'catarina', 'emanuel', 'marcelo', 'claudio', 'marina', 
+'isabela', 'anthony', 'rebeca', 'filipe', 'laís', 'vinicius', 'helena', 'elisa', 'rodrigo', 
+'geraldo', 'yuri', 'marcio'];
+
+function listaNormalClass1(class1) {
+    const len1 = class1.length
+    
+    for (let unsortedIndex = 1; unsortedIndex < len; unsortedIndex++) {
+      let sortedIndex = unsortedIndex
+      const element =class1[unsortedIndex]
+      
+      while (sortedIndex > 0 && class1[sortedIndex - 1] > element) {
+        class1[sortedIndex] = class1[sortedIndex - 1]
+        
+        sortedIndex--
+      }
+
+      class1[sortedIndex] = element1
+      
+    }
+
+    return class1(class1)
+    
+  }
+const class2 = ['benedita', 'elisa', 'emanuel', 'rodrigo', 'filipe', 'marcio', 'teresinha', 'laís', 'vinicius', 
+'marina', 'catarina', 'luiza', 'marcelo', 'rebeca', 'hugo', 'geraldo', 'zeca', 'caio', 'anthony', 
+'yasmin', 'claudio']
+
+function listaNormalClass2(class2) {
+    const len2 = class2.length
+    
+    for (let unsortedIndex = 1; unsortedIndex < len; unsortedIndex++) {
+      let sortedIndex = unsortedIndex
+      const element =class2[unsortedIndex]
+      
+      while (sortedIndex > 0 && class1[sortedIndex - 1] > element) {
+        class2[sortedIndex] = class2[sortedIndex - 1]
+        
+        sortedIndex--
+      }
+
+      class2[sortedIndex] = element1
+      
+    }
+
+    return class2
+    
+  }
+const class3 = ['isadora', 'isabela', 'laís', 'claudio', 'catarina', 'zeca', 
+'teresinha', 'emanuel', 'marcio', 'fatima', 'rodrigo', 'luiza', 'brenda', 'marina', 
+'marcelo', 'benedita', 'rebeca', 'filipe', 'helena', 'elisa', 'hugo', 'geraldo']
+
+function listaNormalClass3(class3) {
+    const len3 = class3.length
+    
+    for (let unsortedIndex = 1; unsortedIndex < len; unsortedIndex++) {
+      let sortedIndex = unsortedIndex
+      const element =class3[unsortedIndex]
+      
+      while (sortedIndex > 0 && class1[sortedIndex - 1] > element) {
+        class3[sortedIndex] = class3[sortedIndex - 1]
+        
+        sortedIndex--
+      }
+
+      class3[sortedIndex] = element1
+      
+    }
+
+    return class3(class3)
+    
+  }
+const class1 = ['yasmin', 'isadora', 'benedita', 'brenda', 'luiza', 'fatima', 'caio', 
+'teresinha', 'hugo', 'zeca', 'catarina', 'emanuel', 'marcelo', 'claudio', 'marina', 
+'isabela', 'anthony', 'rebeca', 'filipe', 'laís', 'vinicius', 'helena', 'elisa', 'rodrigo', 
+'geraldo', 'yuri', 'marcio'];
+
+function listaInvertidaClass1(class1) {
+    const len1 = class1.length
+    
+    for (let unsortedIndex = 1; unsortedIndex < len; unsortedIndex++) {
+      let sortedIndex = unsortedIndex
+      const element =class1[unsortedIndex]
+      
+      while (sortedIndex > 0 && class1[sortedIndex - 1] > element) {
+        class1[sortedIndex] = class1[sortedIndex - 1]
+        
+        sortedIndex--
+      }
+
+      class1[sortedIndex] = element1
+      
+    }
+
+    return class1.reverse(class1)
+    
+  }
+const class2 = ['benedita', 'elisa', 'emanuel', 'rodrigo', 'filipe', 'marcio', 'teresinha', 'laís', 'vinicius', 
+'marina', 'catarina', 'luiza', 'marcelo', 'rebeca', 'hugo', 'geraldo', 'zeca', 'caio', 'anthony', 
+'yasmin', 'claudio']
+
+function listaInvertidaClass2(class2) {
+    const len2 = class2.length
+    
+    for (let unsortedIndex = 1; unsortedIndex < len; unsortedIndex++) {
+      let sortedIndex = unsortedIndex
+      const element =class2[unsortedIndex]
+      
+      while (sortedIndex > 0 && class1[sortedIndex - 1] > element) {
+        class2[sortedIndex] = class2[sortedIndex - 1]
+        
+        sortedIndex--
+      }
+
+      class2[sortedIndex] = element1
+      
+    }
+
+    return class2.reverse(class2)
+    
+  }
+const class3 = ['isadora', 'isabela', 'laís', 'claudio', 'catarina', 'zeca', 
+'teresinha', 'emanuel', 'marcio', 'fatima', 'rodrigo', 'luiza', 'brenda', 'marina', 
+'marcelo', 'benedita', 'rebeca', 'filipe', 'helena', 'elisa', 'hugo', 'geraldo']
+
+function listaInvertidaClass3(class3) {
+    const len3 = class3.length
+    
+    for (let unsortedIndex = 1; unsortedIndex < len; unsortedIndex++) {
+      let sortedIndex = unsortedIndex
+      const element =class3[unsortedIndex]
+      
+      while (sortedIndex > 0 && class1[sortedIndex - 1] > element) {
+        class3[sortedIndex] = class3[sortedIndex - 1]
+        
+        sortedIndex--
+      }
+
+      class3[sortedIndex] = element1
+      
+    }
+
+    return class3.reverse(class3)
+    
+  }
