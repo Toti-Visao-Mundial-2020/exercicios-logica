509c90374b38ea88e7c8243c6e6dbea8cb3bd8fe
Jhoel Gonzalez <jhoel834@gmail.com>
2020-09-29 22:44:23 -0430

    Adicionando arquivo dos exercicios da atividade carros

--- carros/jhoel.js
+++ carros/jhoel.js
@@ -3,29 +3,61 @@ export const nome = "jhoel"
 export function temCarroDaMarca(carros, marca) {
     // coloque aqui o algoritmo para retornar um boolean (true ou false)
     // se existir um carro com a marca informada ou não
-    return null
+    for (let carro of carros) {
+        if (carro.marca === marca) {
+            return true
+        }
+    }
+    return false
 }
 
 export function carrosNaCor(carros, cor) {
     // coloque aqui o algoritmo para retornar a quantidade
     // de carros na cor recebida como parâmetro
-    return null
+    let contador = 0
+    for (let carro of carros) {
+        if (carro.cor === cor) {
+            contador += 1
+        }
+    }
+    return contador
 }
 
 export function anosDisponiveis(carros) {
     // coloque aqui o algoritmo para retornar um array de number
     // com os anos disponíveis nos carros informados
-    return null
+    const disponibleYears = []
+
+    for (let carro of carros) {
+        if (!disponibleYears.includes(carro.ano)) {
+            disponibleYears.push(carro.ano)
+        }
+    }
+    return disponibleYears.sort().reverse()
 }
 
 export function tiposDisponiveis(carros) {
     // coloque aqui o algoritmo para retornar um array de strings
     // com os tipos disponíveis nos carros informados
-    return null
+    const disponibleType = []
+
+    for (let carro of carros) {
+        if (!disponibleType.includes(carro.tipo)) {
+            disponibleType.push(carro.tipo)
+        }
+    }
+    return disponibleType.sort()
 }
 
 export function selecionarCarrosDaMarca(carros, marca) {
     // coloque aqui o algoritmo para retornar um array de object
     // com os carros da marca informado como parâmetro
-    return null
+    const disponibleMark = []
+
+    for (let carro of carros) {
+        if (carro.marca === marca) {
+            disponibleMark.push(carro)
+        }
+    }
+    return disponibleMark
 }
